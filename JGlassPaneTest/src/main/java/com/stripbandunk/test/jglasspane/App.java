bbb125c16c9093f3d5a6006b93747a06c14e2c31
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2011-09-02 13:37:40 +0700

    membuat JGlassPaneTest project

---
+++ JGlassPaneTest/src/main/java/com/stripbandunk/test/jglasspane/App.java
@@ -0,0 +1,13 @@
+package com.stripbandunk.test.jglasspane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
