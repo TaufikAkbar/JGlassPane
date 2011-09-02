e5727df2392a17d8ea6a0274b154e2aca625f2c6
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2011-09-02 12:27:37 +0700

    refactoring TransitionComponent

--- JGlassPane/src/main/java/com/stripbandunk/jglasspane/component/TransitionComponent.java
+++ JGlassPane/src/main/java/com/stripbandunk/jglasspane/component/TransitionComponent.java
@@ -1,26 +1,12 @@
 /*
- * Copyright 2008 echo.khannedy@gmail.com
+ *  Copyright (c) 2011, StripBandunk and/or its affiliates. All rights reserved.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *       http://stripbandunk.com/
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * ["Let's ROCK with JAVA"]
- * 
- * ["echo.khannedy@gmail.com"]
- * ["http://eecchhoo.wordpress.com/"]
+ *  STRIPBANDUNK PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 package com.stripbandunk.jglasspane.component;
 
-import com.stripbandunk.jglasspane.JGlassPane;
 import com.stripbandunk.jglasspane.event.TransitionEvent;
 import com.stripbandunk.jglasspane.event.TransitionListener;
 import com.stripbandunk.jglasspane.transition.Transition;
@@ -31,13 +17,16 @@ import org.jdesktop.animation.timing.Animator;
 import org.jdesktop.animation.timing.interpolation.PropertySetter;
 
 /**
- * TransitionComponent merupakan Komponent yang digunakan untuk menampilkan efek transisi.
- * Komponent ini merupakan komponen yang disimpan dalam {@link JGlassPane} sehingga 
- * dengan mudah {@link JGlassPane} dapat menampilkan efek transisi
  * 
  * @author Eko Kurniawan Khannedy
  */
 public class TransitionComponent extends JComponent {
+
+    public static final float DEFAULT_ACCELERATION = 0.2F;
+
+    public static final float DEFAULT_DECELERATION = 0.4F;
+
+    public static final int DEFAULT_DURATION = 1000;
 
     public static final String PROPERTY_TRANSITION = "PROPERTI_TRANSITION";
 
@@ -47,12 +36,11 @@ public class TransitionComponent extends JComponent {
 
     private int effect;
 
-    private TransitionEvent transitionEvent;
-
     private Transition transition;
 
     public TransitionComponent() {
-        super.setOpaque(false);
+        setOpaque(false);
+        animator = new Animator(0, new PropertySetter(this, "effect", 0, 101));
     }
 
     public Transition getTransition() {
@@ -67,27 +55,38 @@ public class TransitionComponent extends JComponent {
         }
     }
 
-    public void startTransition(int milisecond) {
-        startTransition(milisecond, 0.2F, 0.4F);
+    public void stop() {
+        if (animator.isRunning()) {
+            animator.stop();
+        }
     }
 
-    public void startTransition(int miliSecond, float acelerator, float deceleator) {
-        if (getAnimator().isRunning()) {
-            getAnimator().stop();
+    public void start() {
+        start(TransitionComponent.DEFAULT_DURATION);
+    }
+
+    public void start(int duration) {
+        start(duration, TransitionComponent.DEFAULT_ACCELERATION,
+                TransitionComponent.DEFAULT_DECELERATION);
+    }
+
+    public void start(int duration, float acceleration, float deceleration) {
+        if (animator.isRunning()) {
+            return;
         }
 
-        getAnimator().setDuration(miliSecond);
-        getAnimator().setAcceleration(acelerator);
-        getAnimator().setDeceleration(deceleator);
-        getAnimator().start();
+        animator.setDuration(duration);
+        animator.setAcceleration(acceleration);
+        animator.setDeceleration(deceleration);
+        animator.start();
 
-        fireTransitionListenerOnStart(getTransitionEvent());
+        fireTransitionListenerOnStart(new TransitionEvent(this));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (getTransition() != null && getAnimator().isRunning()) {
+        if (getTransition() != null && animator.isRunning()) {
             paintTransition(g);
         }
     }
@@ -99,13 +98,6 @@ public class TransitionComponent extends JComponent {
         } finally {
             g2.dispose();
         }
-    }
-
-    protected Animator getAnimator() {
-        if (animator == null) {
-            animator = new Animator(0, new PropertySetter(this, "effect", 0, 101));
-        }
-        return animator;
     }
 
     public void addTransitionListener(TransitionListener listener) {
@@ -128,13 +120,6 @@ public class TransitionComponent extends JComponent {
         }
     }
 
-    private TransitionEvent getTransitionEvent() {
-        if (transitionEvent == null) {
-            transitionEvent = new TransitionEvent(this);
-        }
-        return transitionEvent;
-    }
-
     public int getEffect() {
         return effect;
     }
@@ -142,10 +127,9 @@ public class TransitionComponent extends JComponent {
     public void setEffect(int effect) {
         this.effect = effect;
         repaint();
-
         if (effect >= 100) {
-            getAnimator().stop();
-            fireTransitionListenerOnFinish(getTransitionEvent());
+            animator.stop();
+            fireTransitionListenerOnFinish(new TransitionEvent(this));
         }
     }
 }
