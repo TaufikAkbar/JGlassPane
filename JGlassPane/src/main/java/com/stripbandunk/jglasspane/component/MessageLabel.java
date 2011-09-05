3ce744dfc32c273a39073b28d4b5f3c0799444d2
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2011-09-05 11:09:04 +0700

    menambah MessageLabel untuk menampilkan tulisan di MessageComponent

---
+++ JGlassPane/src/main/java/com/stripbandunk/jglasspane/component/MessageLabel.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (c) 2011, StripBandunk and/or its affiliates. All rights reserved.
+ * 
+ *       http://stripbandunk.com/
+ * 
+ *  STRIPBANDUNK PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.stripbandunk.jglasspane.component;
+
+import java.awt.AlphaComposite;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Eko Kurniawan Khannedy
+ */
+class MessageLabel extends JLabel {
+
+    private static final long serialVersionUID = 1L;
+
+    private float alpha = 1.0f;
+
+    public MessageLabel() {
+        setFont(getFont().deriveFont(30f).deriveFont(Font.BOLD));
+    }
+
+    public float getAlpha() {
+        return alpha;
+    }
+
+    public void setAlpha(float alpha) {
+        this.alpha = alpha;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D gd = (Graphics2D) g.create();
+        gd.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        gd.setComposite(AlphaComposite.SrcOver.derive(alpha));
+        super.paintComponent(gd);
+        gd.dispose();
+    }
+}
