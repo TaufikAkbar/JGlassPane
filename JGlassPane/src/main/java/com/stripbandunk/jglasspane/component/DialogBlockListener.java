7384c9248872cd29f011294e90484584998a1fb5
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2011-10-14 12:55:57 +0700

    membuat kelas DialogBlockListener untuk listener agar komponen memblok semua proses dibelakangnya

---
+++ JGlassPane/src/main/java/com/stripbandunk/jglasspane/component/DialogBlockListener.java
@@ -0,0 +1,84 @@
+/*
+ *  Copyright (c) 2011, StripBandunk and/or its affiliates. All rights reserved.
+ * 
+ *       http://stripbandunk.com/
+ * 
+ *  STRIPBANDUNK PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.stripbandunk.jglasspane.component;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import javax.swing.event.MouseInputListener;
+
+/**
+ *
+ * @author Eko Kurniawa Khannedy
+ */
+class DialogBlockListener implements
+        KeyListener,
+        MouseListener,
+        MouseMotionListener,
+        MouseWheelListener,
+        MouseInputListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
